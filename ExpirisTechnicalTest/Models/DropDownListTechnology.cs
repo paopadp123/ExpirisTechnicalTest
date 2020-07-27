4a0acfb07b2b9e740b584548e24c8d7e9fefca1d
Pablo Ospina Polania <Pablo.Ospina@evertecinc.com>
2020-07-26 21:36:51 -0500

    se sube el proyecto al repositorio github

---
+++ ExpirisTechnicalTest/Models/DropDownListTechnology.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpirisTechnicalTest.Models
+{
+    public class DropDownListTechnology
+    {
+        [Required]
+        [Display(Name = "Technology")]
+        public string Technology { get; set; }
+
+        // This property will hold a state, selected by user
+        [Required]
+        [Display(Name = "State")]
+        public string State { get; set; }
+
+        // This property will hold all available states for selection
+        public IEnumerable<SelectListItem> States { get; set; }
+    }
+}
